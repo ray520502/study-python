a12b1706b0c6e26b28fb5518b09fe92b8b05239c
ray520502 <25243818@qq.com>
2017-11-30 01:46:07 -0800

    day3-08-注释

---
+++ day3-08-注释.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+print("hallo world")
+
+#当行注释
+#行首使用#井号可以注释当前行的内容，十七不运行不显示
+#tips在实际代码编译的过程中修改代码不建议直接修改或者删除，而是设置注释
+#因为在某情况中代码往往动一发牵全身，所以不建议直接修改或删除代码行
+
+'''
+print('hallo wrold')
+print('hallo wrold')
+print('hallo wrold')
+print('hallo wrold')
+'''
+#上面一段代码前后使用'''为多行注释，可以使多行代码设为注释
+
+#  :x 为快速保存并退出

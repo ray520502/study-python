f853d6e6e77e4b84bd3b1ecfce00b78a1062e745
ray520502 <25243818@qq.com>
2017-12-11 01:36:15 -0800

    add a new file

---
+++ day3-10定义变量使用变量.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+score = 100 #定义一个变量，这个变量的名字叫score，它里面存储了一个熟知100
+
+high = 180 #单位是cm，定义变量尽量定义一个容易理解的名字
+
+applePrice = 3.5 #苹果的价格 元/斤
+weight = 7.5 #购买的苹果的重量 斤
+
+money = applePrice * weight 
+
+money = money - 10 
+#如果money=XXX是第一次的话那么就是定义一个变量
+#如果money=XXX不是第一次出现的话那就不是定义一个变量，是给一个已有的变量赋予一个新的值
+print(money)

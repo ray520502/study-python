7a6ff4a0ad71339f62ab0488c4414aef3f19c8a7
ray520502 <25243818@qq.com>
2017-12-11 23:34:20 -0800

    查看所有关键词的方法，定义词的驼峰法则

---
+++ day3-14关键词标识符驼峰命名.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+import keyword
+englistScore = 100
+#小驼峰命名
+
+EnglishScore = 100
+#大驼峰命名
+
+#命名定义必须为英文字母（含大小写）数字 下划线，禁止使用# 数字开头 减号等
+
+print(keyword.kwlist) 
+#置入插件import keyword，使用keyword.kwlist可查看所有python3当中的关键词
